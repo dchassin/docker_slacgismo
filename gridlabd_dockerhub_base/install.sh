ccd464563a3baec2836aab1f01f6d15b6ea99443
Alyona Teyber <Ivanova.alyona5@gmail.com>
2022-09-21 10:46:15 -0700

    Update install.sh

--- gridlabd_dockerhub_base/install.sh
+++ gridlabd_dockerhub_base/install.sh
@@ -27,9 +27,9 @@ make install
 
 cd /usr/local/src
 yum install gcc openssl-devel bzip2-devel libffi-devel zlib-devel -y
-curl https://www.python.org/ftp/python/3.9.0/Python-3.9.0.tgz > Python-3.9.0.tgz
-tar xzf Python-3.9.0.tgz 
-cd Python-3.9.0 
+curl https://www.python.org/ftp/python/3.9.6/Python-3.9.6.tgz > Python-3.9.6.tgz
+tar xzf Python-3.9.6.tgz 
+cd Python-3.9.6
 ./configure --prefix=/usr/local --enable-optimizations --with-system-ffi --with-computed-gotos --enable-loadable-sqlite-extensions CFLAGS="-fPIC"
 make -j $(nproc)
 make altinstall
@@ -41,5 +41,5 @@ ln -sf /usr/local/bin/idle3.9 /usr/local/bin/idle
 ln -sf /usr/local/bin/pip3.9 /usr/local/bin/pip3
 /usr/local/bin/python3 -m pip install matplotlib Pillow pandas numpy networkx pytz pysolar PyGithub scikit-learn xlrd boto3
 cd /usr/local/src
-rm -f Python-3.9.0.tgz
+rm -f Python-3.9.6.tgz
 pip3 install --upgrade pip
